8a3f38b91146255892e06d1c064998987c1045b4
srauscher <rauscher-ff3l@t-online.de>
2015-08-13 19:16:53 +0200

    Update to 2015.1.2

--- jenkins.sh
+++ jenkins.sh
@@ -18,10 +18,10 @@
 
 # Globale Einstellungen
 GLUON_URL=https://github.com/freifunk-gluon/gluon.git
-GLUON_COMMIT=v2015.1.1
-#GLUON_BRANCH=experimental
+GLUON_COMMIT=v2015.1.2
+GLUON_BRANCH=experimental
 #GLUON_BRANCH=beta
-GLUON_BRANCH=stable
+#GLUON_BRANCH=stable
 GLUON_PRIORITY=0
 CREATE_TIME=$(date '+%Y-%m-%d-%H-%M')
 
