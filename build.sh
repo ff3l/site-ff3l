aa2bcf088e46c7b5233d0dbd8b002a0095f626a0
David Lutz <kpanic@hirnduenger.de>
2016-04-27 14:26:11 +0200

    Skript überarbeitet BSSIDs angepasst

--- build.sh
+++ build.sh
@@ -10,32 +10,23 @@ catchErr () {
 	fi
 }
 
-Targets='ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-64 x86-xen_domu ramips-rt305x brcm2708-bcm2708 brcm2708-bcm2709 sunxi'
-FirstBranch='testing'
-BrokenBranches='experimental'
+#Targets='ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-64 x86-xen_domu ramips-rt305x brcm2708-bcm2708 brcm2708-bcm2709 sunxi'
+Targets='ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-64'
+BrokenBranches='testing experimental'
 UnBrokenBranches='beta stable'
 
 Doms='1 8'
 
-sed s/---BRANCH---/experimental/ site/site.conf.template >site/site.conf &&\
-
-for Target in $Targets
-do
-	make -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$FirstBranch GLUON_BRANCH=$FirstBranch GLUON_TARGET=$Target V=s | tee log-$Target.log 
-	catchErr
-	make manifest -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$FirstBranch GLUON_BRANCH=$FirstBranch GLUON_TARGET=$Target
-	catchErr
-done
-
-# Images
 
 for Branch in $BrokenBranches
 do
 	sed s/---BRANCH---/$Branch/ site/site.conf.template >site/site.conf &&\
 	for Target in $Targets
 	do
-		make -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
+		echo -ne "\033]0;Main: $Branch $Target\007"
+		make images -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
 		catchErr
+		echo -ne "\033]0;Main: $Branch $Target Manifest\007"
 		make manifest -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
 		catchErr
 	done
@@ -46,8 +37,10 @@ do
 	sed s/---BRANCH---/$Branch/ site/site.conf.template >site/site.conf &&\
 	for Target in $Targets
 	do
-		make -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
+		echo -ne "\033]0;Main: $Branch $Target\007"
+		make images -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
 		catchErr
+		echo -ne "\033]0;Main: $Branch $Target Manifest\007"
 		make manifest -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
 		catchErr
 	done
@@ -57,13 +50,15 @@ done
 
 for Dom in $Doms
 do
-	for Branch in $FirstBranch $BrokenBranches
+	for Branch in $BrokenBranches
 	do
 		sed s/---BRANCH---/$Branch/ site/site.conf.$Dom.template >site/site.conf &&\
 		for Target in $Targets
 		do
-			make -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'dom$Dom/$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
+			echo -ne "\033]0;$Dom: $Branch $Target\007"
+			make images -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'dom$Dom/$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
 			catchErr
+			echo -ne "\033]0;$Dom: $Branch $Target Manifest\007"
 			make manifest -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'dom$Dom/$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
 			catchErr
 		done
@@ -74,8 +69,10 @@ do
 		sed s/---BRANCH---/$Branch/ site/site.conf.$Dom.template >site/site.conf &&\
 		for Target in $Targets
 		do
-			make -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'dom$Dom/$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
+			echo -ne "\033]0;$Dom: $Branch $Target\007"
+			make images -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'dom$Dom/$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
 			catchErr
+			echo -ne "\033]0;$Dom: $Branch $Target Manifest\007"
 			make manifest -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'dom/$Dom/$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
 			catchErr
 		done
