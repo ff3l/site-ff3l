9d30371ed0ef206d5498639206f98c004f715766
David Lutz <kpanic@hirnduenger.de>
2016-05-30 09:44:45 +0200

    Build-Version generalisiert

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ catchErr () {
 Targets='ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-64'
 BrokenBranches='testing experimental'
 UnBrokenBranches='beta stable'
+Ver='v2016.1.5+001'
 
 Doms='hoho wtk wald wiese loe 3land ref test'
 
@@ -24,11 +25,11 @@ do
 	for Target in $Targets
 	do
 		echo -ne "\033]0;Main: $Branch $Target\007"
-		make -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
+		make -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target GLUON_RELEASE=$Ver
 		catchErr
 	done
 	echo -ne "\033]0;Main: $Branch Manifest\007"
-	make manifest -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch
+	make manifest -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_RELEASE=$Ver
 	catchErr
 done
 
@@ -38,11 +39,11 @@ do
 	for Target in $Targets
 	do
 		echo -ne "\033]0;Main: $Branch $Target\007"
-		make -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
+		make -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target GLUON_RELEASE=$Ver
 		catchErr
 	done
 	echo -ne "\033]0;Main: $Branch Manifest\007"
-	make manifest -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch
+	make manifest -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Branch GLUON_BRANCH=$Branch GLUON_RELEASE=$Ver
 	catchErr
 done
 
@@ -56,11 +57,11 @@ do
 		for Target in $Targets
 		do
 			echo -ne "\033]0;$Dom: $Branch $Target\007"
-			make -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Dom/$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
+			make -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Dom/$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target GLUON_RELEASE=$Ver
 			catchErr
 		done
 		echo -ne "\033]0;$Dom: $Branch Manifest\007"
-		make manifest -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Dom/$Branch GLUON_BRANCH=$Branch
+		make manifest -j17 BROKEN=1 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Dom/$Branch GLUON_BRANCH=$Branch GLUON_RELEASE=$Ver
 		catchErr
 	done
 
@@ -70,11 +71,11 @@ do
 		for Target in $Targets
 		do
 			echo -ne "\033]0;$Dom: $Branch $Target\007"
-			make -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Dom/$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target
+			make -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Dom/$Branch GLUON_BRANCH=$Branch GLUON_TARGET=$Target GLUON_RELEASE=$Ver
 			catchErr
 		done
 		echo -ne "\033]0;$Dom: $Branch Manifest\007"
-		make manifest -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Dom/$Branch GLUON_BRANCH=$Branch
+		make manifest -j17 BROKEN=0 GLUON_IMAGEDIR='$(GLUON_OUTPUTDIR)/'$Dom/$Branch GLUON_BRANCH=$Branch GLUON_RELEASE=$Ver
 		catchErr
 	done
 done
