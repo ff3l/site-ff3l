33c1b8adf6483d19e87fdf3a53eac68258a14d8b
HACKER-3000 <arvidwohrle@gmail.com>
2018-04-25 15:23:57 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -10,7 +10,6 @@ gluon-config-mode-hostname \
 gluon-config-mode-mesh-vpn \
 gluon-ebtables-filter-multicast \
 gluon-ebtables-filter-ra-dhcp \
-gluon-ebtables-segment-mld \
 gluon-ebtables-source-filter \
 gluon-web-admin \
 gluon-web-autoupdater \
