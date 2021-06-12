f1817e479037154ed1f7f01ecd63a5f9d280772a
Felix <git@xdfr.de>
2021-06-12 17:00:03 +0200

    Removed: deprecated variable

--- site.mk
+++ site.mk
@@ -35,8 +35,6 @@ GLUON_SITE_PACKAGES := \
 
 DEFAULT_GLUON_RELEASE := g$(shell git -C $(shell pwd) log --pretty=format:'%h' -n 1)+s$(shell git -C $(shell pwd)/site log --pretty=format:'%h' -n 1)~$(shell date '+%Y%m%d')
 
-GLUON_ATH10K_MESH ?= 11s
-
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
